649f8a594f6f608b51f523ed83f05fa8cfd49b01
Vedhanth <gvedhanth123@outlook.com>
2022-12-02 13:28:59 +0530

    Create Iterate_over_Array.java

---
+++ Iterate_over_Array.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Iterate_over_Array {
+  
+    public static void main(String[] args) {
+      
+        // creating a 1 dimensional array
+        int[] array = {1,2,3,4,5};
+        
+        // iterating over an array
+        for(int i = 0; i < array.length; i ++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
